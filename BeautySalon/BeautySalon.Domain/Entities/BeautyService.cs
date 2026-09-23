f05d156ce8d2269a9d2ddb4d02dc1400026a7a7b
exhaustion666 <crowford-1337@mail.ru>
2026-09-23 20:25:28 +0400

    created beauty service class

---
+++ BeautySalon/BeautySalon.Domain/Entities/BeautyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeautySalon.Domain.Entities;
+
+public class BeautyService
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Category { get; set; }
+    public required decimal Price { get; set; }
+    public required TimeSpan Duration { get; set; }
+}
