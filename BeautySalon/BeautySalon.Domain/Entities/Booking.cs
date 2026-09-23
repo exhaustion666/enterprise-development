5405ed49dab2d8b428d899d1dce85773042a66be
exhaustion666 <crowford-1337@mail.ru>
2026-09-23 20:38:03 +0400

    created class of an appointment for the beauty service

---
+++ BeautySalon/BeautySalon.Domain/Entities/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeautySalon.Domain.Entities;
+
+public class Booking
+{
+    public required int Id { get; set; }
+    public required DateTimeOffset StartAt { get; set; }
+    public required int SpecialistId { get; set; }
+    public Specialist? Specialist { get; set; }
+    public required int CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+    public required int BeautyServiceId { get; set; }
+    public BeautyService? BeautyService { get; set; }
+    public required bool IsRegularCustomer { get; set; }
+}
